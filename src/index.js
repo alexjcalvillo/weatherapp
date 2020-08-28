20a3c0dea03cec7ae8f5cba75e9a34697af137b5
Alexander Calvillo <alexanderjcalvillo@gmail.com>
2020-08-28 08:30:51 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App.js';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+
+// Create the rootSaga generator function
+function* rootSaga() {}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Example saga setup
+// const movies = (state = [], action) => {
+//     switch (action.type) {
+//         case 'SET_MOVIES':
+//             return action.payload;
+//         default:
+//             return state;
+//     }
+// }
+
+// Create one store that all components can use
+const storeInstance = createStore(
+  combineReducers({
+    movies,
+    genres,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
